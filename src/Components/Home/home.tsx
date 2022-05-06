03070d1c74f5777c884644e5c44b69eef3f884c0
Niranjan K V <Niranjan.KV@BRILLIO.COM>
2022-05-06 18:15:11 +0530

    first commit

---
+++ src/Components/Home/home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return(
+        <div>
+            <h3 className="text-center">Welcome to the vaccination Home page</h3>
+            <br /><br />
+            
+        </div>
+    )
+}
+export default Home;
